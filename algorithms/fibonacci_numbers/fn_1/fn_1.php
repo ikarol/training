a8c719455267cb5eaee9ad64c9cbe3f741dd1999
Ivan Karol <ikarol@gomel.iba.by>
2017-03-12 17:52:50 +0300

    removed extra variables

--- algorithms/fibonacci_numbers/fn_1/fn_1.php
+++ algorithms/fibonacci_numbers/fn_1/fn_1.php
@@ -8,9 +8,7 @@ For operations with big numbers use BCMath module.
 
 require_once 'speed_test.php';
 
-$symbols = rand(100,1000);
-echo 'Symbols number is ' . $symbols . "\n";
-$stats = [];
+echo 'Symbols number is 100' . "\n";
 
 /**
  * Function that gets first Fibonacci number based on given amount of number's symbols
@@ -18,16 +16,13 @@ $stats = [];
  * @param  integer $range  amount of number's symbols
  * @return integer         first number with $range symbols of number
  */
-function fibonacci_arr($range = 100)
+function fibonacci_arr()
 {
     $result = [0,1];
     $i = 2;
-    $count = 0;
-    while($count != $range) {
+    while(strlen($result[count($result)-1]) != 100) {
         $result[] = bcadd($result[$i-1], $result[$i-2]);
         $i++;
-        $last_el = count($result)-1;
-        $count = strlen($result[$last_el]);
     }
     return $result[count($result)-1];
 }
@@ -38,17 +33,15 @@ function fibonacci_arr($range = 100)
  * @param  integer $range  amount of number's symbols
  * @return integer         first number with $range symbols of number
  */
-function fibonacci_loop($range = 100)
+function fibonacci_loop()
 {
     $x = 0;    
     $y = 1; 
     $z = 0;
-    $count = 0;
-    while($count != $range) { 
+    while(strlen($z) != 100) { 
         $z = bcadd($x, $y);
         $x = $y;
         $y = $z;
-        $count = strlen($z);
     }
     return $z;
 }
@@ -60,31 +53,36 @@ function fibonacci_loop($range = 100)
  * @return integer         first number with $range symbols of number
  */
 // Works slowly with big numbers
-function fibonacci_gr($range = 100)
+function fibonacci_gr()
 {
     $x = 1;    
     $y = 1; 
     $z = 0;
-    $count = 0;
     $i = 2;
     define('gr', 1.618034);
-    while($count != $range) { 
+    while(strlen($z) != 100) { 
         $z = (int)(bcsub(pow(gr, $i),pow(bcsub(1, gr,6), $i),6) / sqrt(5));
         $x = $y;
         $y = $z;
         $i++;
-        $count = strlen($z);
     }
     return $z;
 }
 
 echo "--------------Array solution------------------\n";
-$stats[] = speedTest('fibonacci_arr', $symbols);
-
+$as_st = microtime(true);
+echo 'Function result: ' . fibonacci_arr() . "\n";
+$as_et = microtime(true) - $as_st;
+echo 'Time: ' . $as_et . "\n";
 echo "--------------Loop solution------------------\n";
-$stats[] = speedTest('fibonacci_loop', $symbols);
-
+$ls_st = microtime(true);
+echo 'Function result: ' . fibonacci_loop() . "\n";
+$ls_et = microtime(true) - $ls_st;
+echo 'Time: ' . $ls_et . "\n";
 // works slowly with big numbers
 // echo "--------------Golden Ratio solution-------------\n";
-// $sta
-// ts[] = speedTest('fibonacci_gr', $symbols);
+// $grs_st = microtime(true);
+// echo 'Function result: ' . fibonacci_gr() . "\n";
+// $grs_et = microtime(true) - $grs_st;
+// echo 'Time: ' . $grs_et . "\n";
+
--- algorithms/fibonacci_numbers/fn_1/speed_test.php
+++ algorithms/fibonacci_numbers/fn_1/speed_test.php
@@ -6,12 +6,11 @@
  * @param  Array $funcArg array of arguments
  * @return Array          array of function stats(memory usage and perfomance time)
  */
-function speedTest(callable $func,...$funcArg)
+function speedTest(callable $func)
 {
-    $args = implode(',', $funcArg);
     $startMem = memory_get_usage();
     $startTime = microtime(true);
-    echo 'function result: ' . $func($args) . "\n";
+    echo 'function result: ' . $func() . "\n";
     $finalMem = memory_get_usage() - $startMem;
     $finalTime = number_format((microtime(true) - $startTime), 4);
     echo 'Memory usage: ' . $finalMem . " bytes\n";
