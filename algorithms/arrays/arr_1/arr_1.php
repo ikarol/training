26883aa5a5235f5b12ed881a2e3374cac3182a63
Ivan Karol <ikarol@gomel.iba.by>
2017-03-13 10:11:46 +0300

    ic

---
+++ algorithms/arrays/arr_1/arr_1.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Matrix n*n is given.
+Each cell contains positive numbers.
+Matrix should be filled randomly by script.
+
+The task is to find the shortest path from cell (0,0) to cell (n,n).
+Shortest path is not the number of steps,but the sum of the numbers from cells
+that are on the scripts path.
+
+The matrix and highlighted path with the sum of the numbers should be displayed
+in browser.
+ */
+
+
